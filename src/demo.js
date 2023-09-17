5a511e0f8f6432f324f33180e50b87f7157a2fa3
Sam <sam@sambhattacharyya.com>
2023-09-17 09:43:08 -0500

    Got a basic version working with lyra encoded and decoded

--- src/demo.js
+++ src/demo.js
@@ -6,4 +6,50 @@ import {encodeWithLyra, decodeWithLyra, isLyraReady} from 'lyra-codec'
 
 console.log("Hello world");
 
+
+window.process = function (data) {
+
+
+    return new Promise(function (resolve) {
+
+        console.log("Got pcm 16 data");
+
+        console.log(data);
+
+
+        setTimeout(function () {
+
+
+            if(isLyraReady()){
+
+                console.log("Ready to process");
+
+                const encodedAudio = encodeWithLyra(data, 8000);
+                console.log("Encoded audio");
+                console.log(encodedAudio);
+
+                const decoded = decodeWithLyra(encodedAudio, 8000, data.length);
+
+                resolve(decoded);
+
+            } else {
+                console.log("Not ready");
+            }
+
+
+        }, 1000);
+
+
+
+
+    })
+
+
+
+
+
+
+
+}
+
 //console.log(isLyraReady);
